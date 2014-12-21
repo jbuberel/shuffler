0b96f8c0b8068e7440d3ca41da297eb3cf3752fb
jbuberel <jason@buberel.org>
2014-12-20 16:59:19 -0800

    Implementation using function pointers

--- src/github.com/jbuberel/shuffler.go
+++ src/github.com/jbuberel/shuffler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"time"
 )
 
 type Deck []int
@@ -11,54 +12,61 @@ type Deck []int
 var (
 	numDecks int = 1000
 	numCards int = 52
+	trials int = 100
+	r *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 )
 
 func main() {
-	r := rand.New(rand.NewSource(99))
-	decks := make([]Deck, numDecks)
 
-	// generate 1000 decks, and shuffle them	
-	for d := 0; d < len(decks); d++ {
-	
-		cards := make(Deck, numCards)
-		
-		// populate the deck with sequential entries
-		for i := 0; i < numCards; i++ {
-			cards[i] = i
+	var fairCount int
+
+	fairCount = 0
+	for i := 0; i < trials; i++ {
+		if isFair(randomShuffle) {
+			fairCount++
 		}
-		
-		nullShuffle(r, cards)
-		
-		// store each shuffled deck
-		decks[d] = cards
-		
 	}
+	fmt.Printf("randomShuffler %v/%v with %v decks\n", fairCount, trials, numDecks)
 	
-	fmt.Printf("Is null shuffle fair? %v\n", isFair(decks))
-	
-	// generate 1000 decks, and shuffle them	
-	for d := 0; d < len(decks); d++ {
-	
-		cards := make(Deck, numCards)
-		
-		// populate the deck with sequential entries
-		for i := 0; i < numCards; i++ {
-			cards[i] = i
+	fairCount = 0
+	for i := 0; i < trials; i++ {
+		if isFair(halfAssedShuffler) {
+			fairCount++
 		}
-		
-		randomShuffle(r, cards)
-		
-		// store each shuffled deck
-		decks[d] = cards
-		
 	}
+	fmt.Printf("halfAssedShuffler %v/%v with %v decks\n", fairCount, trials, numDecks)
 	
-	fmt.Printf("Is random shuffle fair? %v\n", isFair(decks))
+	
+	fairCount = 0
+	for i := 0; i < trials; i++ {
+		if isFair(nullShuffle) {
+			fairCount++
+		}
+	}
+	fmt.Printf("nullShuffle %v/%v with %v decks\n", fairCount, trials, numDecks)
+	
 }
 
-func randomShuffle(r *rand.Rand, cards Deck) {
-	
+func randomShuffle(cards Deck) {
+
 	for i := 0; i < numCards; i++ {
+		j := r.Intn(numCards)
+		t := cards[i]
+		cards[i] = cards[j]
+		cards[j] = t
+	}
+
+}
+
+func nullShuffle(cards Deck) {
+	return
+}
+
+func halfAssedShuffler(cards Deck) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	for i := 0; i < numCards; i += 2 {
 		j := r.Intn(numCards)
 		t := cards[i]
 		cards[i] = cards[j]
@@ -67,30 +75,49 @@ func randomShuffle(r *rand.Rand, cards Deck) {
 	
 }
 
-func nullShuffle(r *rand.Rand, cards Deck) {
-	return
-}
+type shuffle func( cards Deck)
+
+
 
 // For each position in the set of decks, this will compute the
 // average value and the standard deviation. A shuffle is considered
 // unfair if the average for any position is greater than half
 // the standard deviation away from the nominal mean (numCards/2).
-func isFair (decks []Deck) (bool) {
-	
+//func isFair (decks []Deck) (bool) {
+func isFair(shuf shuffle) bool {
+	decks := make([]Deck, numDecks)
+
+	// generate 1000 decks, and shuffle them
+	for d := 0; d < len(decks); d++ {
+
+		cards := make(Deck, numCards)
+
+		// populate the deck with sequential entries
+		for i := 0; i < numCards; i++ {
+			cards[i] = i
+		}
+
+		shuf(cards)
+
+		// store each shuffled deck
+		decks[d] = cards
+
+	}
+
 	for i := 0; i < numCards; i++ {
 		sum := 0
 		stdDev := 0.0
 		for j := 0; j < numDecks; j++ {
 			sum += decks[j][i]
-			stdDev += (float64(numCards/2) - float64(decks[j][i])) * (float64(numCards/2) - float64(decks[j][i])) 
+			stdDev += (float64(numCards/2) - float64(decks[j][i])) * (float64(numCards/2) - float64(decks[j][i]))
 		}
-		stdDev = math.Sqrt(stdDev/float64(numDecks))
-		avg := float64(sum/numDecks)
-		
-		if math.Abs(avg - float64(numCards/2)) > stdDev/2 {
+		stdDev = math.Sqrt(stdDev / float64(numDecks))
+		avg := float64(sum / numDecks)
+
+		if math.Abs(avg-float64(numCards/2)) > stdDev/2 {
 			return false
 		}
 	}
-	
+
 	return true
 }
